b6a10c1a3332770105df9946a88c0ba621031cd8
Liviu Diaconu <liviudiaconu@appnet.dev>
2024-09-11 12:18:18 +0200

    main

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 sudo cd /var/www/html
 sudo supervisorctl stop all
+git add .
+git commit -m "Guardando cambios locales antes de rebase"
+git pull --rebase origin main
+# (resuelve conflictos si los hay)
+git rebase --continue
+git push origin main
+
+php artisan migrate
+
 npm update
 composer update
 rm -rf /etc/supervisor/conf.d/*
@@ -7,5 +16,3 @@ cp laravel*.conf /etc/supervisor/conf.d/
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart all
-
-php artisan migrate
