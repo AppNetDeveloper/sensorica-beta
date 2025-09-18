4905aeecfde2b3cd8eaab639da25320252910ef0
AppNetDeveloper <liviudiaconu@appnet.dev>
2025-09-18 14:14:26 +0200

    chore: update success message with ellipsis in update script

--- update.sh
+++ update.sh
@@ -424,4 +424,4 @@ echo 'limpiar cache'
     php artisan db:seed --class=ProductionLineOrdersKanbanPermissionSeeder --force -n
     php artisan db:seed --class=WorkCalendarPermissionSeeder --force -n
     php artisan db:seed --class=OriginalOrderFilePermissionSeeder --force -n
-    echo "Proceso completado con éxito."
+    echo "Proceso completado con éxito..."
